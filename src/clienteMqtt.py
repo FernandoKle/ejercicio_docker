a3ded621dff216267f6accfba0e9d5b4daf94ce8
Fernando Kleinubing <kleinubing84@gmail.com>
2024-04-18 16:54:05 -0300

    algo

--- src/clienteMqtt.py
+++ src/clienteMqtt.py
@@ -5,16 +5,7 @@ import asyncio, ssl, certifi, logging, os, sys
 import aiomqtt
 from contextvars import ContextVar
 
-WorkerName = ContextVar('worker_name')
-
-logging.basicConfig(format='%(asctime)s: servicio_mqtt (%(worker_name)s) - %(levelname)s -> %(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S %z')
-
-class WorkerAdapter(logging.LoggerAdapter):
-    def process(self, msg, kwargs):
-        kwargs.setdefault('extra', {})['worker_name'] = WorkerName.get()
-        return msg, kwargs
-
-logger = WorkerAdapter(logging.getLogger(__name__), None)
+logging.basicConfig(format='%(asctime)s: servicio_:mqtt (%(thread)s) - %(levelname)s -> %(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S %z')
 
 async def main():
     tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
@@ -29,11 +20,11 @@ async def main():
     ) as client:
         await client.subscribe(os.environ['TOPICO_1'])
         async for message in client.messages:
-            logger.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
+            logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
 
 if __name__ == "__main__":
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        logger.info("Terminando ejecucion")
+        logging.info("Terminando ejecucion")
         sys.exit(0)
